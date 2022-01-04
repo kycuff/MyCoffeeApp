a54b34e1ee4d20fef02b9701f9f71f0172e22ad5
kycuff <kylecuthbert32@gmail.com>
2022-01-04 19:47:30 +0000

    Added click event with counter
    Counter added to the page with MVVM

---
+++ MyCoffeeApp/MyCoffeeApp/ViewModels/CoffeeEquipmentViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MyCoffeeApp.ViewModels
+{
+    public class CoffeeEquipmentViewModel : BindableObject
+    {
+        public CoffeeEquipmentViewModel()
+        {
+            IncreaseCount = new Command(OnIncrease);
+        }
+
+        public ICommand IncreaseCount { get; }
+
+        int count = 0;
+
+        private string _countDisplay;
+
+        public string CountDisplay
+        {
+            get => _countDisplay;
+            set
+            {
+                if (value == _countDisplay)
+                    return;
+
+                _countDisplay = value;
+                OnPropertyChanged(nameof(CountDisplay));
+            }
+        }
+
+        void OnIncrease()
+        {
+            count++;
+            CountDisplay = $"you clicked {count} times(s)";
+        }
+    }
+}
